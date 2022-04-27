c9e78703187cbb8986a8386010c1c7ac633293db
Daniel Catania <dcataniarodrigues20@gmail.com>
2022-04-27 16:15:55 -0300

    movendo código da home para pasta screens

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../patterns/header";
+import Footer from "../../patterns/footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main style={{backgroundColor: "red"}} />
+      <Footer />
+    </>
+  );
+}
