be3142cd5f907998c9680b264b47d53b6568b834
Daniel Catania <dcataniarodrigues20@gmail.com>
2022-04-28 21:57:21 -0300

    refatorando o header

--- src/patterns/header/Header.tsx
+++ src/patterns/header/Header.tsx
@@ -4,11 +4,6 @@ const Header = styled.header`
   /* cor */
   background-color: ${({theme}) => theme.colors.primary.c300};
 
-  /* posicionamento */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
   h1 {
     /* cor */
     color: ${({theme}) => theme.colors.primary.c50};
--- src/patterns/header/index.tsx
+++ src/patterns/header/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import BoxHeader from "./Header";
 import Title from "../../components/Title";
+import Centralize from "../../components/Centralize";
 
 export default function Header() {
   return (
     <BoxHeader>
-      <img src="/img/flash.svg" alt="Logo do The Flash News" />
-      <Title>The Flash News</Title>
+      <Centralize>
+        <img src="/img/flash.svg" alt="Logo do The Flash News" />
+        <Title>The Flash News</Title>
+      </Centralize>
     </BoxHeader>
   );
 }
