cf0510e432f8448c4628ce03e57056f8768842c8
Daniel Catania <dcataniarodrigues20@gmail.com>
2022-04-28 21:48:13 -0300

    refatorando página 404

--- src/screens/404/index.tsx
+++ src/screens/404/index.tsx
@@ -1,26 +1,37 @@
 import React from "react";
+import styled from "styled-components";
 import Link from "../../components/Link";
 import Head from "../../patterns/head";
 import Header from "../../patterns/header";
 import Footer from "../../patterns/footer";
-import Main from "./Main404";
 import Title from "../../components/Title";
 import Subtitle from "../../components/Subtitle";
+import Centralize from "../../components/Centralize";
+
+const Contents = styled.div`
+  /* posicionamento */
+  text-align: center;
+
+  /* tamanho */
+  width: 70%;
+`;
 
 export default function page404() {
   return (
     <>
       <Head pageTitle="404" />
       <Header />
-      <Main>
-        <div>
-          <Title>404</Title>
-          <Subtitle>
-            Ops!! Parece que você foi rápido demais e acabou se perdendo!
-          </Subtitle>
-          <Link href="/">Voltar ao início</Link>
-        </div>
-      </Main>
+      <main>
+        <Centralize>
+          <Contents>
+            <Title>404</Title>
+            <Subtitle>
+              Ops!! Parece que você foi rápido demais e acabou se perdendo!
+            </Subtitle>
+            <Link href="/">Voltar ao início</Link>
+          </Contents>
+        </Centralize>
+      </main>
       <Footer />
     </>
   );
