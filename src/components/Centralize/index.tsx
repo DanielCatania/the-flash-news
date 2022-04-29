c40aa23414f0be529d4a36ab1ff5e3db068dbe7f
Daniel Catania <dcataniarodrigues20@gmail.com>
2022-04-28 21:20:21 -0300

    criando componente de centralização

---
+++ src/components/Centralize/index.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Centralize = styled.div`
+  /* posicionamento */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  /* tamanho */
+  width: 100%;
+  height: 100%;
+`;
+
+export default Centralize;
