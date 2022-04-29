4e5f97eb7eaaabfde3a00046b29767d2d4aeb4cd
Daniel Catania <dcataniarodrigues20@gmail.com>
2022-04-28 22:00:22 -0300

    criando componente de enfileiramento

---
+++ src/components/QueuedList/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const QueuedList = styled.ul`
+  /* posicioanmento */
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export default QueuedList;
