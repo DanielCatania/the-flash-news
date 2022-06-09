e7021e300ecd48909f7712b3b75422ecd518b012
Daniel Catania <dcataniarodrigues20@gmail.com>
2022-06-08 22:50:40 -0300

    conexão com a api de clima

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
+import {GetServerSideProps} from "next";
 import Home from "../src/screens/home";
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const apiKeyClimate = process.env.API_KEY_CLIMATE;
+
+  const climate = await (
+    await fetch(`https://api.hgbrasil.com/weather?key=${{apiKeyClimate}}`)
+  ).json();
+
+  return {
+    props: {
+      climate: climate.results,
+    },
+  };
+};
 
 export default Home;
